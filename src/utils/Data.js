fb8e6f5f5d0cb679ae332068082974dc1b058727
aromal chandran <aromalchandran912@gmail.com>
2023-02-22 02:25:30 -0800

    Initial commit

---
+++ src/utils/Data.js
@@ -0,0 +1,54 @@
+export const books = [
+  {
+    id: 1,
+    name: "Master your emotions",
+    author: "joseph stephen",
+    price: "120",
+    imgUrl: "https://m.media-amazon.com/images/I/81BgEsl0SJL._AC_UY218_.jpg",
+  },
+  {
+    id: 2,
+    name: "Aatujeevitham: Vol. 1",
+    author: " BENYAMIN",
+    price: 123,
+    imgUrl:
+      "https://images-na.ssl-images-amazon.com/images/I/51NQaV3F1uL._SX322_BO1,204,203,200_.jpg",
+  },
+  {
+    id: 3,
+    name: "Randamoozham ",
+    author: " M T Vasudevan Nair",
+    price: 120,
+    imgUrl:
+      "https://images-na.ssl-images-amazon.com/images/I/41fRojGGDUL._SX319_BO1,204,203,200_.jpg",
+  },
+  {
+    id: 4,
+    name: "Alchemist (Malayalam)",
+    author: " Paulo Coelho ",
+    price: 150,
+    imgUrl:
+      "https://images-na.ssl-images-amazon.com/images/I/41tm1PWX2fL._SX319_BO1,204,203,200_.jpg",
+  },
+  {
+    id: 5,
+    name: "Atomic Habits",
+    author: "James Clear",
+    imgUrl:
+      "https://i.gr-assets.com/images/S/compressed.photo.goodreads.com/books/1535115320l/40121378._SY475_.jpg",
+  },
+  {
+    id: 7,
+    name: "Dune",
+    author: "Frank Herbert",
+    imgUrl:
+      "https://i.gr-assets.com/images/S/compressed.photo.goodreads.com/books/1434908555l/234225._SY475_.jpg",
+  },
+  {
+    id: 8,
+    name: "Atlas Shrugged",
+    author: "Ayn Rand",
+    imgUrl:
+      "https://i.gr-assets.com/images/S/compressed.photo.goodreads.com/books/1405868167l/662.jpg",
+  },
+];
